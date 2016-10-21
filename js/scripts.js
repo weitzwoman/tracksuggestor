f37d4e1084978eb932ec8987006c6a530d986e7b
Tracie Weitzman <tlweitzman@gmail.com>
2016-10-21 10:13:57 -0700

    save work on scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,42 @@
+// var language = function(q1, q2, q3, q4, q5) {
+//   if (total <= 11) {
+//
+//   return "You might enjoy taking CSS/Design";
+//
+// } else if (total > 11 && total <= 18) {
+//
+//   return "You might enjoy Ruby/Rails";
+//
+// } else if (total > 19 && total <= 25) {
+//
+//    return "You might enjoy C#/.NET";
+//
+// }
+// }
+
+  if (total <= 11) {
+    $(".langCSS").show();
+    } else if (total > 11 && total <= 18) {
+      $(".langRuby").show();
+    } else if (total > 19 && total <= 25) {
+      $(".langCsharp").show();
+}
+
+
 $(document).ready(function() {
-  $("form").submit(function(event) {
-//     var personName = $("input#fullname").val();
-//     var address = $("input#address").val();
-//     var pizza = $("#pizza").val();
-//
-//     $(".first").text(personName);
-//     $(".second").text(address);
-//     $(".third").text(pizza);
-//     $(".jumbotron").show();
-//     event.preventDefault();
-//   });
-// });
-//
-//
-// var triangle = function(side1, side2, side3) {
-//   if (side1 === side2 && side1 === side3 && side1 === side2) {
-//
-//   console.log("equil attempt");
-//   return "This is an equilateral triangle!";
-//
-// } else if (side1 === side2 || side1 === side3) {
-//
-//   return "This is an isoceles triangle!";
-//   console.log("iso attempt");
-//
-// } else if (side1 != side2 && side1 != side3 && side2 != side3) {
-//
-//    return "This is an scalene triangle!";
-//    console.log("scalene attempt");
-//
-// } else if (side1 === side2 && side1 === side3) {
-//
-//   return "This is NOT a triangle!";
-//   console.log("NOT attempt");
-//
-// } else {
-//   return "test";
-// }
-// }
-//
-//
-//
-// $(document).ready(function() {
-//   $("form").submit(function(event) {
-//     event.preventDefault();
-//     var side1 = parseInt($("input#side1").val());
-//     var side2 = parseInt($("input#side2").val());
-//     var side3 = parseInt($("input#side3").val());
-//
-//     var t1 = triangle(side1, side2, side3);
-//     $("#t1").text(t1);
+  $("form#survey").submit(function(event) {
+    var company = parseInt($("#company").val());
+    var work = parseInt($("#work").val());
+    var food = parseInt($("#food").val());
+    var people = parseInt($("#people").val());
+    var city = parseInt($("#city").val());
+    var total = company + work + food + people + city;
+
+  event.preventDefault();
+
+
+    // var language = total;
+    // $("#language").text(language);
+
+  });
+});
